d51c34ee63c5fceb1b19d868262ac8ebae8da4f3
Nico Tuider <3515@htl.rennweg.at>
2017-10-17 09:11:26 +0200

    flag circle added

---
+++ src/Flag_Circle.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Tuider\Flags;
+
+class Flag_Circle extends Flag implements FlagInterface {
+    private $radius;
+
+    /**
+     * Flag_4 constructor.
+     * @param string $bezeichnung
+     * @param string $farbe
+     * @param int $radius
+     */
+    public function __construct(string $bezeichnung, string $farbe, int $radius)
+    {
+        parent::__construct($bezeichnung, $farbe);
+        $this->radius = $radius;
+    }
+
+    /**
+     * @return float
+     */
+    public function getArea():float{
+        return Round((pi() * pow($this->radius, 2)), 2);
+    }
+}
